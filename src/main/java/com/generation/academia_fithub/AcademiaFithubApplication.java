f8e7ed79fef4f72169048677a7b043f9555f6466
Yanrato <yanferreira985@gmail.com>
2026-07-31 11:36:23 -0300

    :tada: Commit Inicial

---
+++ src/main/java/com/generation/academia_fithub/AcademiaFithubApplication.java
@@ -0,0 +1,13 @@
+package com.generation.academia_fithub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AcademiaFithubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AcademiaFithubApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/generation/academia_fithub/AcademiaFithubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.generation.academia_fithub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AcademiaFithubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
